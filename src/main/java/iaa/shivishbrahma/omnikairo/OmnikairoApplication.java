62c4b531cd114a7e0db93b7ef6189953aedd6600
Purbayan Chowdhury <pur.cho.99@gmail.com>
2024-12-20 00:14:08 +0530

    first commit

---
+++ src/main/java/iaa/shivishbrahma/omnikairo/OmnikairoApplication.java
@@ -0,0 +1,13 @@
+package iaa.shivishbrahma.omnikairo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnikairoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnikairoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iaa/shivishbrahma/omnikairo/OmnikairoApplicationTests.java
@@ -0,0 +1,13 @@
+package iaa.shivishbrahma.omnikairo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnikairoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
